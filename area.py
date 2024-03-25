48ba252fb5241ad9da5dea016215fbf03f899e92
melibonilla <136756568+melibonilla@users.noreply.github.com>
2024-03-25 17:18:52 -0600

    Add files via upload

---
+++ area.py
@@ -0,0 +1,17 @@
+#Declaración de variables
+base=0
+altura=0
+
+
+#Input de datos
+base= int(input("Ingrese la medida de la base: "))
+altura= int(input("Ingrese la medida de la altura: "))
+
+
+#Proceso de datos
+perimetro = 2*(base+altura)
+area = base*altura
+
+
+#Output de datos
+resultado= print("El perímetro es: ", perimetro, " y el área es: ", area)
